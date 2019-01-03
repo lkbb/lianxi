ac0165449e701d8ef75892d4f4b9daa0e763c530
likuo <17600013561@163.com>
2019-01-03 09:10:41 +0800

    图片上传功能

---
+++ likuo/src/App.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="wrap">
+    <div class="head">
+      <img class="head_pic" src="./assets/zuo.png" alt="">
+      <span>补换驾照</span>
+      <img class="head_pic" src="./assets/gengduo.png" alt="">
+    </div>
+    <div class="section">
+      
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  
+}
+</script>
+<style lang="scss">
+.wrap{
+  width: 100%;
+  height: 100%;
+  padding: 0;
+  margin: 0;
+  display: flex;
+  flex-direction: column;
+  .head{
+    width: 100%;
+    height: 45px;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    border-bottom: 1px solid #999;
+    .head_pic{
+      width: 25px;
+      height: 25px;
+    }
+  }
+  .section{
+    width: 100%;
+    flex-shrink: 1;
+    flex: 1;
+
+  }
+}
+</style>
